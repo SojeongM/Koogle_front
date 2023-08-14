765e373c3593857f9f9ac5322fb4435ca8b060c5
Sehwan-Chang <kdwkdwkd@naver.com>
2023-08-14 21:33:28 +0900

    테스트용 app.js

--- koogle/src/App.js
+++ koogle/src/App.js
@@ -4,14 +4,15 @@ import Header from "./components/Header";
 import Navigators from "./components/Navigate";
 import Filter from "./components/Filter";
 import Main2 from "./components/Main2";
-import Main3 from "./components/Main3";
+import Review from "./components/Review";
+import Info from "./components/Info";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Navigators></Navigators>
-      <Main3></Main3>
+      <Review></Review>
     </div>
   );
 }
